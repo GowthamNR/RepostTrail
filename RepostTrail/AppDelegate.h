5291e8c4fdeea4def93046853b542c85577fba7a
Bifortis <bifortis@Bifortiss-Mac-mini-3.local>
2015-07-27 10:57:31 +0530

    Initial Commit

---
+++ RepostTrail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RepostTrail
+//
+//  Created by Bifortis on 7/27/15.
+//  Copyright (c) 2015 Bifortis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
